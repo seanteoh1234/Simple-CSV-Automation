77184a5cae416b83fab74b34849fc8482c491140
seanteoh1234 <92786434+seanteoh1234@users.noreply.github.com>
2023-03-24 21:31:39 +0800

    Update Simple CSV Automation.py

--- Simple CSV Automation.py
+++ Simple CSV Automation.py
@@ -1,10 +1,10 @@
-with open('E:/members.csv', 'r') as file:
+with open('insert all members csv here', 'r') as file:
     # Read all the lines of the file into a list
     lines = file.readlines()
 
 # Filter out the inactive members and write them to the inactive file
-with open('E:/inactive.csv', 'a') as inactive_file:
-    with open('E:/members.csv', 'w') as members_file:
+with open('insert inactive members csv here', 'a') as inactive_file:
+    with open('insert all members csv here, 'w') as members_file:
         for line in lines:
             if 'inactive' in line.lower():
                 inactive_file.write(line)
@@ -13,7 +13,7 @@ with open('E:/inactive.csv', 'a') as inactive_file:
 
 import pandas as pd
 
-a = pd.read_csv('E:/members.csv')
+a = pd.read_csv('insert all members csv here')
 empty_status = a.loc[a['Status:'].isnull()]
 if len(empty_status) > 1:
     print("There are " + str(len(empty_status['Name:'])) + " names with empty status. Please check")
